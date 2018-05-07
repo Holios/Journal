736c758769c8b084e7b4bac6bcbe5583206957e7
Unknown <Thraide@gmail.com>
2018-05-07 07:09:21 -0400

    update to methods and re-write of primary code

--- journal.py
+++ journal.py
@@ -4,7 +4,7 @@
 import sys
 import datetime
 import time
-import pickle
+import os
 
 class Entry:
     def __init__(self):
@@ -13,7 +13,7 @@ class Entry:
 
 
 selection = ""
-entries = {}
+
 while(True):
     print ("1: Add a journal entry")
     print ("2: read previous journal entries")
@@ -22,36 +22,30 @@ while(True):
     selection = input(">")
 
     if selection == "1":
+# will create a local folder for year, and sub folders for months, will auto
+# create new monthly folders and will create a file with input from the user
+# will use the datetime year and datetime month to ensure it is saving entries
+# to a searchable folder structure of year(folder) > month(subfolder) > entry(file)
+# entry will be listed by date and hour, if there is an entry within the last hour
+# todo: will ask the user to either rename, or overwrite
+        year_folder = "{datetime.datetime.now().strftime('%Y')}"
+        month_folder = "{datetime.datetime.now().strftime('%M')}"
         time = datetime.datetime.now().strftime("%d-%m-%y-%h")
-
-# check to see if there is a current entry for today
-        if (time in entries.keys()):
-            print ("there is already a very recent entry")
-            print ("do you want to over write the recent entry?")
-            overwrite = input("> (Y/N)")
-            if overwrite == "Y":
-                print ("please enter the new entry")
-                entries[time].text = input(">")
-                p_out = open("journal.pkl", "wb")
-                pickle.dump(entries[time].text, p_out)
-            if overwrite == "N":
-                print ("returning to main menu")
-        else:
-            print ("enter your journal entry for today")
-            entries[time] = Entry()
-
-
-
-
+        if os.path.isdir(f'./{datetime.datetime.now().strftime("%Y")}') == False:
+            mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime("%Y"))
+            os.makedirs(mydir)
+        if os.path.isdir(f'./{year_folder} + "\\" + {month_folder}') == False:
+            mydir2 = os.path.join(os.getcwd(), datetime.datetime.now().strftime("%Y") + "//" + datetime.datetime.now().strftime('%B'))
+            os.makedirs(mydir2)
+        if os.path.is
+        print("Enter in \"x\" alone to exit.")
+        while(True):
+            newline = input(": ")
+            if(newline=="x"):
+                break
+            else: journal = journal + (newline + "\n")
 
     if selection == "2":
-        print ("these are the previous entries")
-        p_in = open("journal.pkl", "rb")
-        returned_entries = pickle.load(p_in)
-        print (returned_entries)
-
-
-
-
+        pass
     if selection == "4":
         sys.exit()
