e21d6a49faa3455cb7bf12b5c695e073022c9811
Unknown <Thraide@gmail.com>
2018-05-11 08:25:59 -0400

    version 1.0 working final for now

--- journal.py
+++ journal.py
@@ -16,8 +16,6 @@ selection = ""
 
 while(True):
     print ("1: Add a journal entry")
-    print ("2: read previous journal entries")
-    print ("3: Delete/modify an entry")
     print ("4: exit program")
     selection = input(">")
 
@@ -29,7 +27,13 @@ while(True):
 # entry will be listed by date and hour, if there is an entry within the last hour
 # todo: will ask the user to either rename, or overwrite
 
+
         journal = ""
+        journal_entry = ""
+        mood = ""
+        mood_entry = ""
+        questionnaire_format = "\nSelf Questionnaire\n------------\n"
+
         mydir = datetime.datetime.now().strftime('%Y')
         mydir2 = datetime.datetime.now().strftime('%B')
 
@@ -44,19 +48,71 @@ while(True):
         os.chdir(mydir2)
 
         my_file_name = datetime.datetime.now().strftime("%B - %d")
-        my_file = open(my_file_name + '.txt', "w")
-        print("Enter in \"x\" alone to exit.")
+        my_file = open(my_file_name + '.txt', "a")
+
+        print("how are you feeling today? (Enter in \"x\" alone to exit.)")
+        while (True):
+            mood_new_line = input(": ")
+            if(mood_new_line == "x"):
+                break
+            else: mood = mood + (mood_new_line + "\n")
+        mood_entry = (f"MOOD: {mood}  \n------------ \n \n")
+        my_file.write(mood_entry)
+
+        print("what is your journal entry for today? \n(Enter in \"x\" alone to exit.)")
+        journal = ""
         while(True):
             newline = input(": ")
             if(newline=="x"):
                 break
             else: journal = journal + (newline + "\n")
-        my_file.write(journal)
+        journal_entry = (f"Journal Entry for {datetime.datetime.now().strftime('%B-%d')} \n\n   {journal}")
+        my_file.write(journal_entry)
+
+        my_file.write(questionnaire_format)
+
+        print("What was the best thing that happened today? \n(Enter in \"x\" alone to exit.)")
+        journal = ""
+        while(True):
+            newline = input(": ")
+            if(newline =="x"):
+                break
+            else: journal = journal + (newline + "\n")
+        journal_entry = (f"The best thing that happened today was: \n{journal}\n")
+        my_file.write(journal_entry)
+
+        print("Name one thing you did for someone else today. \n(Enter in \"x\" alone to exit.)")
+        journal = ""
+        while(True):
+            newline = input(": ")
+            if(newline=="x"):
+                break
+            else: journal = journal + (newline + "\n")
+        journal_entry = (f"Today I helped someone else by:\n{journal}\n")
+        my_file.write(journal_entry)
+
+        print("Name one thing you did for yourself today: \n(Enter in \"x\" alone to exit.)")
+        journal = ""
+        while(True):
+            newline = input(": ")
+            if(newline=="x"):
+                break
+            else: journal = journal + (newline + "\n")
+        journal_entry = (f"Today I helped myself by:\n{journal}\n")
+        my_file.write(journal_entry)
+
+        print("Name one thing you learned or accomplished today: \n(Enter in \"x\" alone to exit.)")
+        journal = ""
+        while(True):
+            newline = input(": ")
+            if(newline=="x"):
+                break
+            else: journal = journal + (newline + "\n")
+        journal_entry = (f"Today I:\n{journal}\n")
+        my_file.write(journal_entry)
         my_file.close()
 
         os.chdir("../..")
 
-    if selection == "2":
-        pass
     if selection == "4":
         sys.exit()
